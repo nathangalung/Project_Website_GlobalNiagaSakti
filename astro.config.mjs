53cc33a18a8e2813f8721c92553cc708afa80c04
nathangalung <18222130@std.stei.itb.ac.id>
2025-02-24 20:49:26 +0700

    update: english indo

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,15 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   output: 'server',
   adapter: vercel({
-    analytics: true,
+    webAnalytics: {
+      enabled: true,
+    },
+    imageService: true,
+    imagesConfig: {
+      sizes: [640, 750, 828, 1080, 1200, 1920],
+      domains: [],
+      minimumCacheTTL: 60,
+    },
   }),
   integrations: [tailwind()]
 });
